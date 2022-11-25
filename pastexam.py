f96ea29be940e1ed7d0ef382f49aa76d95eba7ef
tonystark <76855867+mercurytony@users.noreply.github.com>
2022-11-25 15:52:43 +1000

    Delete pastexam.py

--- pastexam.py
+++
@@ -1,9 +0,0 @@
-def has_multiple(string,char):
-    char_present = False 
-    for c in string:
-        if c == char:
-            if len(c + c) >= 2:
-                return True
-        else:
-            char_present = True
-    return False 
